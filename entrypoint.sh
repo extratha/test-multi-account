d5c77b5021d8d080d11e9117de1e106979d6ff84
Chisanupong Sirijan <chisanupongs@cariva.co.th>
2024-06-04 11:49:32 +0000

    add firebase env

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,50 @@ find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0
 find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
 find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
 
+# Start Firebase
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_APP_ID#$NEXT_PUBLIC_FIREBASE_APP_ID#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#$NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID#g"
+# End Firebase
+
 echo "Environment variables replaced."
 
 exec "$@"
--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { staticEnvConfig } from "@/constant/env";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbhlK4qGnGaYTnoe_b1Ax1NsqVhu2y6Fg",
-  authDomain: "cariva-playground-dev.firebaseapp.com",
-  projectId: "cariva-playground-dev",
-  storageBucket: "cariva-playground-dev.appspot.com",
-  messagingSenderId: "239529071608",
-  appId: "1:239529071608:web:5d31b10f8145102e82bc0d",
-  measurementId: "G-QGY9TN7LQ1"
+  apiKey: staticEnvConfig.firebase.apiKey,
+  authDomain: staticEnvConfig.firebase.authDomain,
+  projectId: staticEnvConfig.firebase.projectId,
+  storageBucket: staticEnvConfig.firebase.storageBucket,
+  messagingSenderId: staticEnvConfig.firebase.messagingSenderId,
+  appId: staticEnvConfig.firebase.appId,
+  measurementId:staticEnvConfig.firebase.measurementId
 };
 
 // Initialize Firebase
 export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-export const analytics = getAnalytics(app);
+
+export const analytics = (typeof window !== "undefined") ? getAnalytics(app) : undefined;
