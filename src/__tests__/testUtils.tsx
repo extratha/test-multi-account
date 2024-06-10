58f01ca84b6fa38ddaabbcfbdb8e14a4f24d1a8e
Chakkrit Chueangyang <chakkritc@cariva.co.th>
2024-06-10 07:32:47 +0000

    fix/package-version

--- src/__tests__/testUtils.tsx
+++ src/__tests__/testUtils.tsx
@@ -1,10 +1,11 @@
-import { theme } from "@/config/config-mui/theme";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import { RenderOptions, RenderResult, act, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import flushPromises from "flush-promises";
 import { NextIntlClientProvider } from "next-intl";
 import { SWRConfig } from "swr";
+
+import { theme } from "@/config/config-mui/theme";
 import aiInterpretEn from "../../public/locales/en/aiInterpret.json";
 import aiInterpretTh from "../../public/locales/th/aiInterpret.json";
 import { default as commonEn, default as commonTh } from "../../public/locales/th/common.json";
@@ -38,11 +39,12 @@ const renderWithProviders: RenderWithProvider = (component, renderOptions?, loca
   });
 };
 
-export * from "@testing-library/react";
-export { renderWithProviders as render, userEvent };
 
 export const flushPromise = async () => {
   await act(async () => {
     await flushPromises();
   });
 };
+
+export * from "@testing-library/react";
+export { renderWithProviders as render, userEvent };
