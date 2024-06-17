fe03fc0484d964bb3a12034c08726b1d204a8bae
thakorn.t <thakorn.t@codemonday.com>
2024-06-17 10:04:39 +0700

    update unit test : FormAutocomplete, InputDataModule

--- src/modules/InputDataModule/InputDataModule.test.tsx
+++ src/modules/InputDataModule/InputDataModule.test.tsx
@@ -1,22 +1,27 @@
-import { useRouter, useSearchParams } from "next/navigation";
+import useInterpretInputDataConfig from "@/hooks/useInterpretInputDataConfig";
+import "firebase/remote-config";
+import { useRouter } from "next/navigation";
 import InputDataModule from ".";
-import { render, screen, userEvent } from "../../__tests__/testUtils";
-
+import { render, renderHook, screen, userEvent } from "../../__tests__/testUtils";
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
-  useSearchParams: jest.fn(),
+  useSearchParams: jest.fn().mockReturnValue({ id: "" }),
+}));
+jest.mock("firebase/app", () => ({
+  getApps: jest.fn().mockReturnValue([]),
+  initializeApp: jest.fn(),
+}));
+jest.mock("firebase/analytics", () => ({
+  getAnalytics: jest.fn(),
 }));
 
+jest.mock("firebase/remote-config", () => ({
+  getRemoteConfig: jest.fn(() => ({ settings: { minimumFetchIntervalMillis: 60000 } })),
+  fetchAndActivate: jest.fn().mockResolvedValue(""),
+  getValue: jest.fn().mockReturnValue({ asString: jest.fn().mockReturnValue("[]") }),
+}));
 describe("InputDataModule", () => {
   it("should render correctly", () => {
-    const mockUseSearchParams = useSearchParams as jest.Mock;
-    mockUseSearchParams.mockReturnValue({
-      get: jest.fn().mockImplementation((key) => {
-        if (key === "id") return "";
-        return null;
-      }),
-    });
-
     const { asFragment } = render(<InputDataModule />);
     expect(asFragment()).toMatchSnapshot();
   });
@@ -24,6 +29,10 @@ describe("InputDataModule", () => {
   it("call router back when click back", async () => {
     const mockBack = jest.fn();
     const mockUseRouter = useRouter as jest.Mock;
+    const { result } = renderHook(() => useInterpretInputDataConfig());
+    // act(()=> {
+    //   result.current.
+    // })
     mockUseRouter.mockReturnValue({
       back: mockBack,
     });
