7a9422f2f1c7fb9c644c575c9e9617fc7bf48bf7
Chakkrit Chueangyang <chakkritc@cariva.co.th>
2024-07-01 06:21:18 +0000

    feature/NV-42-dependency

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,12 @@
 
 echo "Replacing environment variables..."
 
-find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_API_BASE_PATH#$NEXT_PUBLIC_API_BASE_PATH#g"
-
-find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
-find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
-find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
-find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
-find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
-find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_DOMAIN_URL#$NEXT_PUBLIC_BASE_DOMAIN_URL#g"
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_BASE_API_URL#$NEXT_PUBLIC_BASE_API_URL#g"
 
 # Start Firebase
 find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_API_KEY#$NEXT_PUBLIC_FIREBASE_API_KEY#g"
@@ -30,6 +23,13 @@ find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | 
 find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
 find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
 find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN#g"
+
+find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
+find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
+find /app/.next/server/app \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
+find /app/.next/static/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
+find /app/.next/required-server-files.json \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
+find /app/server.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_PROJECT_ID#$NEXT_PUBLIC_FIREBASE_PROJECT_ID#g"
 
 find /app/.next/server/chunks \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
 find /app/.next/server/pages \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#MOCK_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET#g"
