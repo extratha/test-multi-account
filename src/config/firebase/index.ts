aa6e7145278ad1192524b2b84166995f92909816
Chakkrit Chueangyang <chakkritc@cariva.co.th>
2024-07-03 08:52:49 +0000

    feature/NV-163-refactor-playground

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { envConfig } from "@/constant/env";
 import { getAnalytics } from "firebase/analytics";
-import { staticEnvConfig } from "@/constant/env";
+import { getApps, initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,16 +9,16 @@ import { staticEnvConfig } from "@/constant/env";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: staticEnvConfig.firebase.apiKey,
-  authDomain: staticEnvConfig.firebase.authDomain,
-  projectId: staticEnvConfig.firebase.projectId,
-  storageBucket: staticEnvConfig.firebase.storageBucket,
-  messagingSenderId: staticEnvConfig.firebase.messagingSenderId,
-  appId: staticEnvConfig.firebase.appId,
-  measurementId:staticEnvConfig.firebase.measurementId
+  apiKey: envConfig.firebase.apiKey,
+  authDomain: envConfig.firebase.authDomain,
+  projectId: envConfig.firebase.projectId,
+  storageBucket: envConfig.firebase.storageBucket,
+  messagingSenderId: envConfig.firebase.messagingSenderId,
+  appId: envConfig.firebase.appId,
+  measurementId: envConfig.firebase.measurementId,
 };
 
 // Initialize Firebase
 export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export const analytics = (typeof window !== "undefined") ? getAnalytics(app) : undefined;
+export const analytics = typeof window !== "undefined" ? getAnalytics(app) : undefined;
--- src/utils/axios/login.ts
+++ src/utils/axios/login.ts
@@ -1,26 +1,25 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
-import { staticEnvConfig } from '@/constant/env';
+import { envConfig } from "@/constant/env";
+import axios, { AxiosError, AxiosResponse } from "axios";
 const axiosPublicInstance = axios.create({
-  baseURL: staticEnvConfig.apiUrl,
-  
-})
+  baseURL: envConfig.apiUrl,
+});
 axiosPublicInstance?.interceptors.request.use(
   async (config) => {
     const newConfig = config;
-    delete config.headers.Authorization
+    delete config.headers.Authorization;
     return newConfig;
   },
-  (error) => { 
-    console.log(error)
-  },
+  (error) => {
+    console.log(error);
+  }
 );
 axiosPublicInstance?.interceptors.response.use(
-    (response: AxiosResponse) => response,
-    (error: AxiosError) => {
-      return Promise.reject(error.response?.data  || 'Something went wrong');
-    },
-  );
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => {
+    return Promise.reject(error.response?.data || "Something went wrong");
+  }
+);
 
-export * from 'axios';
+export * from "axios";
 
 export default axiosPublicInstance;
