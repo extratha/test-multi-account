11fa6e834fec4325ebb0c406fbfc11486d0ae992
Thakorn Thongrak <thakornt@cariva.co.th>
2024-07-17 06:23:12 +0000

    fix/NV-259-validate-fields

--- src/modules/InputDataModule/index.tsx
+++ src/modules/InputDataModule/index.tsx
@@ -195,7 +195,7 @@ const InputDataModule = () => {
     if (inputData.length > 0 && defaultInputData) {
       trigger();
     }
-  }, [inputData.length]);
+  }, [inputData.length, isLoading]);
 
   return (
     <>
