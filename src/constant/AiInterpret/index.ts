ac5fa7ca296b88908e2c33b52cce2444c12d9164
Thakorn Thongrak <thakornt@cariva.co.th>
2024-08-01 06:23:14 +0000

    feature/NV-338-change-autocomplete-placeholder

---
+++ src/constant/AiInterpret/index.ts
@@ -0,0 +1,17 @@
+export const FIELD_NAME = {
+  GENDER: "gender",
+  STOOL_COLOR: "stool_color",
+  STOOL_WBC: "stool_wbc",
+  STOOL_RBC: "stool_rbc",
+  STOOL_PARASITE: "stool_parasite",
+  STOOL_OCCULT_BLOOD: "stool_occult_blood",
+  STOOL_CULTURE: "stool_culture",
+  URINE_COLOR: "urine_color_value",
+  URINE_SP_GR_VALUE: "urine_sp_gr_value",
+  URINE_PH_VALUE: "urine_ph_value",
+  URINE_WBC_VALUE: "urine_wbc_value",
+  URINE_PROTEIN_VALUE: "urine_protein_value",
+  URINE_GLUCOSE_VALUE: "urine_glucose_value",
+  URINE_ERYTHROCYTE_VALUE: "urine_erythrocyte_value",
+  URINE_RBC_VALUE: "urine_rbc_value",
+};
